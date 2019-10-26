55d5bd34aa8bbeec41a193c03ded666f6ed43d6e
Hermann Rosch <Hermann.Rosch@ennuba.com>
2019-10-26 00:10:16 -0500

    Hasta la implementación de RolesController.cs con EF

---
+++ Migrations/20191026044301_SeedRoleData.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository.API.Migrations
+{
+    public partial class SeedRoleData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                  @"INSERT [Role] ([Id], [Name]) VALUES (1, N'Administrador')
+                    INSERT [Role] ([Id], [Name]) VALUES (2, N'Captador de Datos')
+                    INSERT [Role] ([Id], [Name]) VALUES (3, N'Reporteador')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE FROM [Role]");
+        }
+    }
+}
